126b1e2b3ff760826814e99d9d428fde9f47709f
hisecure <wq@dsa>
2025-04-01 16:50:35 +0900

    dsa

--- bfs.py
+++ bfs.py
@@ -1,21 +1,20 @@
 from dfs import my_graph
 
-
 def my_bfs(graph, start_node):
     visit = list()
     queue = list()
 
     visit.append(start_node) # 방문한 노드들
-    child_node = graph[start_node] # 방문한 노드의 자식노드를 저장함
-    queue.extend(child_node) # 뒤집은 자식노드를 스택에 추가함
+    queue.extend(start_node) # 뒤집은 자식노드를 큐에 추가함
 
     while queue: # 모든 노드를 방문할때까지
         print(f"queue : {queue}")
-        node = queue.pop(0)
-        print(f"현재 방문한 node : {node}")
+        node = queue.pop(0) # 큐에서 아래서부터 꺼냄
         if node not in visit:
+            print(f"현재 방문한 node : {node}")
             visit.append(node) # 방문한 노드를 추가
             queue.extend(graph[node]) # 자식노드를 큐에 추가함
+
     return visit
 
 
--- dfs.py
+++ dfs.py
@@ -20,19 +20,18 @@ def my_dfs(graph, start_node):
     stack = list()
 
     visit.append(start_node) # 방문한 노드들
-    child_node = graph[start_node] # 방문한 노드의 자식노드를 저장함
-    child_node.reverse() # 자식노드를 뒤집음
-    stack.extend(child_node) # 뒤집은 자식노드를 스택에 추가함
+    graph[start_node].reverse() # 자식 노드를 뒤집음
+    stack.extend(graph[start_node]) # 뒤집은 자식노드를 스택에 추가함
 
     while stack: # 모든 노드를 방문할때까지
         print(f"stack : {stack}")
-        node = stack.pop()
-        print(f"현재 방문한 node : {node}")
+        node = stack.pop() # 스택에서 위에서부터 꺼냄
+
         if node not in visit:
+            print(f"현재 방문한 node : {node}")
             visit.append(node) # 방문한 노드를 추가
-            child_node = graph[node] # 방문한 노드의 자식노드를 저장함
-            child_node.reverse() # 자식노드를 뒤집음
-            stack.extend(child_node) # 자식노드를 스택에 추가함
+            graph[node].reverse() # 방문한 노드의 자식노드를 저장함
+            stack.extend(graph[node]) # 자식노드를 스택에 추가함
     return visit
 
 
